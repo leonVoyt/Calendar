43dc32aba8e9d4d00f8940a7ddf4df2dfce1119e
Leon <leonvoytkevichus@gmail.com>
2024-10-01 14:51:04 +0300

    init

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -1,77 +1,135 @@
-// src/components/CalendarComponent.js
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import EventForm from "./EventForm";
-// import { DragDropContext } from "react-dnd";a
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "./styles.css";
 
 const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
 
 const CalendarComponent = () => {
   const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [formOpen, setFormOpen] = useState(false);
-
+  // Handle selecting time slots
   const handleSelectSlot = (slotInfo) => {
     setFormOpen(true);
     setSelectedEvent({
-      start: slotInfo.start,
-      end: slotInfo.end,
+      start: moment(slotInfo.start).startOf("day").toDate(),
+      end: moment(slotInfo.start).endOf("day").toDate(),
       title: "",
-      color: "#3174ad",
+      color: "#B02222",
+      allDay: false,
     });
   };
 
+  // Handle clicking on an event
   const handleEventClick = (event) => {
     setSelectedEvent(event);
     setFormOpen(true);
   };
 
+  // Handle saving an event from the EventForm
   const handleSaveEvent = (newEvent) => {
     setEvents((prevEvents) => {
-      const isExisting = prevEvents.find((e) => e === selectedEvent);
+      const isExisting = prevEvents.find(
+        (e) => e.title === selectedEvent.title
+      );
       if (isExisting) {
-        return prevEvents.map((e) => (e === selectedEvent ? newEvent : e));
+        return prevEvents.map((e) =>
+          e.title === selectedEvent.title ? newEvent : e
+        );
       }
+      localStorage.setItem(
+        "storageEvents",
+        JSON.stringify([...prevEvents, newEvent])
+      );
+
       return [...prevEvents, newEvent];
     });
     setFormOpen(false);
   };
+  useEffect(() => {
+    const eventsFromStore = localStorage.getItem("storageEvents");
+    if (eventsFromStore) {
+      const parseIvents = JSON.parse(eventsFromStore);
 
+      setEvents(
+        parseIvents?.map((el) => ({
+          ...el,
+          start: moment(el.start).toDate(),
+          end: moment(el.end).toDate(),
+        }))
+      );
+    }
+  }, []);
+  // Handle deleting an event
   const handleDeleteEvent = () => {
-    setEvents((prevEvents) => prevEvents.filter((e) => e !== selectedEvent));
+    setEvents((prevEvents) =>
+      prevEvents.filter((e) => e.title !== selectedEvent.title)
+    );
+    localStorage.setItem(
+      "storageEvents",
+      JSON.stringify(events.filter((e) => e.title !== selectedEvent.title))
+    );
     setFormOpen(false);
   };
 
+  // Handle dragging events to a new position
+  const onEventDrop = ({ event, start, end }) => {
+    const idx = events.indexOf(event);
+    const updatedEvent = { ...event, start, end, color: event.color };
+    const nextEvents = [...events];
+    nextEvents.splice(idx, 1, updatedEvent);
+
+    localStorage.setItem("storageEvents", JSON.stringify(nextEvents));
+
+    setEvents(nextEvents);
+  };
+
+  // Handle resizing events by dragging
+  const onEventResize = ({ event, start, end }) => {
+    const nextEvents = events.map((existingEvent) => {
+      return existingEvent.id === event.id
+        ? { ...existingEvent, start, end, color: event.color }
+        : existingEvent;
+    });
+    localStorage.setItem("storageEvents", JSON.stringify(nextEvents));
+    setEvents(nextEvents);
+  };
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="calendar-container">
-        <Calendar
-          localizer={localizer}
-          events={events}
-          selectable
-          onSelectSlot={handleSelectSlot}
-          onSelectEvent={handleEventClick}
-          style={{ height: "100vh" }}
-          eventPropGetter={(event) => ({
-            style: { backgroundColor: event.color },
-          })}
-          defaultView="month"
+    <div className="calendar-container">
+      <DnDCalendar
+        localizer={localizer}
+        events={events}
+        step={30}
+        selectable
+        showMultiDayTimes
+        onDragOver={(e) => e.preventDefault()}
+        defaultView="month"
+        eventPropGetter={(event) => ({
+          style: { backgroundColor: event.color },
+        })}
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={handleEventClick}
+        onEventDrop={onEventDrop}
+        onEventResize={onEventResize}
+        resizable
+        onDropFromOutside={onEventDrop}
+        style={{ height: "100vh" }}
+      />
+      {formOpen && (
+        <EventForm
+          event={selectedEvent}
+          onSave={handleSaveEvent}
+          onDelete={handleDeleteEvent}
+          onClose={() => setFormOpen(false)}
         />
-        {formOpen && (
-          <EventForm
-            event={selectedEvent}
-            onSave={handleSaveEvent}
-            onDelete={handleDeleteEvent}
-            onClose={() => setFormOpen(false)}
-          />
-        )}
-      </div>
-    </DndProvider>
+      )}
+    </div>
   );
 };
 
--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -1,20 +1,70 @@
-// src/components/EventForm.js
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ChromePicker } from "react-color";
+import moment from "moment";
 
-const EventForm = ({ event, onSave, onDelete, onClose }) => {
+// Use default parameters for the props
+const EventForm = ({
+  event = {},
+  onSave = () => {},
+  onDelete = () => {},
+  onClose = () => {},
+}) => {
   const [title, setTitle] = useState(event.title || "");
-  const [color, setColor] = useState(event.color || "#3174ad");
+  const [color, setColor] = useState(event.color);
 
+  useEffect(() => {
+    setStart(moment(event.start).format("YYYY-MM-DDTHH:mm"));
+    setEnd(moment(event.end).format("YYYY-MM-DDTHH:mm"));
+  }, [event]);
+
+  // Initialize start and end date states with event data or default to the current time
+  const [start, setStart] = useState(
+    moment(event.start).format("YYYY-MM-DDTHH:mm") ||
+      moment().format("YYYY-MM-DDTHH:mm")
+  );
+  const [end, setEnd] = useState(
+    moment(event.end).format("YYYY-MM-DDTHH:mm") ||
+      moment().add(1, "hours").format("YYYY-MM-DDTHH:mm")
+  );
+
+  // Handle saving the event with the updated title, color, start, and end times
   const handleSave = () => {
-    const updatedEvent = { ...event, title, color };
+    const updatedEvent = {
+      ...event,
+      title,
+      color,
+      start: moment(start).toDate(),
+      end: moment(end).toDate(),
+    };
     onSave(updatedEvent);
   };
 
   return (
     <div className="event-form">
       <h2>{event.title ? "Edit Event" : "Add New Event"}</h2>
+
+      <div>
+        <label>
+          <strong>Start:</strong>
+          <input
+            type="datetime-local"
+            value={start}
+            onChange={(e) => setStart(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <strong>End:</strong>
+          <input
+            type="datetime-local"
+            value={end}
+            onChange={(e) => setEnd(e.target.value)}
+          />
+        </label>
+      </div>
+
       <input
         type="text"
         value={title}
@@ -22,13 +72,25 @@ const EventForm = ({ event, onSave, onDelete, onClose }) => {
         onChange={(e) => setTitle(e.target.value)}
         placeholder="Event name (max 30 chars)"
       />
+
       <ChromePicker
         color={color}
         onChangeComplete={(newColor) => setColor(newColor.hex)}
       />
-      <button onClick={handleSave}>Save</button>
-      {event.title && <button onClick={onDelete}>Delete</button>}
-      <button onClick={onClose}>Cancel</button>
+
+      <div className="event-form__btn--container">
+        <button className="save" onClick={handleSave}>
+          Save
+        </button>
+        {event.title && (
+          <button className="delete" onClick={onDelete}>
+            Delete
+          </button>
+        )}
+        <button className="cancel" onClick={onClose}>
+          Cancel
+        </button>
+      </div>
     </div>
   );
 };
